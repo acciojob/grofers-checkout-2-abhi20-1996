1d6440b072c286c4873e26f3bc76e6adbd002615
abhi20-1996 <110686880+abhi20-1996@users.noreply.github.com>
2023-06-09 11:04:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+let tableEl=document.getElemrntById("myTable");
+let priceArr=Array.from(document..querySelectorAll("td[data-ns-test]"));
+let totalPrice = 0;
+for (let i = 0; i < priceArr.length; i++) {
+	totalPrice+=priceArr[i];
+}
+let totalRow=document.creatElement("tr");
+let newCell=totalRow.insertCell(0);
+totalRow.setAttribute('data-ns-test', 'grandTotal');
+tableEl.insertAdjacentElement('beforeend', totalRow);
